d73f9cdd579de08dbaccfb4e379f44b23f64c63f
DamenW <93298899+DamenW@users.noreply.github.com>
2022-01-24 12:00:52 -0600

    Create DamenW_Lab4.py

---
+++ DamenW_Lab4.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+#create a dataframe (data table)
+df = pd.DataFrame(columns = ['risk', 'likelihood', 'impact', 'score'])
+
+#identify list of uncertainties
+df['risk'] = ['users', 'team', 'budget', 'security']
+
+#estimate the likelihood of happening
+df ['likelihood'] = [0.2, 0.3, 0.1, 0.5]
+
+#assess the severity of impact if the uncertainty happens (pick a scale)
+df['score'] = df['impact']*df['likelihood']
+df['impact'] = [1, 2, 7, 10]
+
+#calculate scores
+df['score'] = df['impact']*df['likelihood']
+
+#create the heatmap
+scoresPivot = df.pivot('impact', 'likelihood', 'score')
+print(scoresPivot)
+
+labelsPivot = df.pivot('impact', 'likelihood', 'risk')
+print(labelsPivot)
+
+#replaces no labels with empty strings
+labelsPivot.fillna('', inplace=True)
+
+#simple heatmap
+p1 = sns.heatmap(scoresPivot, cmap="BuPu", annot=labelsPivot, fmt='')
